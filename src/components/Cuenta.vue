5344414c3453ecac268d63e83ae770b335a0151f
Pablo Feijo <pablofeijo181@gmail.com>
2021-04-06 08:53:59 -0300

    segundo commit

--- src/components/Cuenta.vue
+++ src/components/Cuenta.vue
@@ -1,19 +1,91 @@
 <template>
+    <div class="container">
+        <h2>{{titular}}</h2>
+        <hr>
+        <h3>Cuenta: {{cuenta}}</h3>
+        <h3>Saldo: {{saldo}}</h3>
+        <h3>Estado: {{estado ? 'Cuenta Activa' : 'Cuenta Desactivada'}}</h3>
+        <h4>Servicios Disponibles</h4>
+        <div v-for="(item, index) in transacciones" :key="index">
+           {{index+1}}--{{ item }}
+        </div>
+        <p :hidden="info">Saldo insuficiente</p>
+    </div>
     <div>
+        <ModificarSaldo @signal="agregarSaldo" class="boton" nombre="Añadir Saldo"></ModificarSaldo>
+        <ModificarSaldo :desactivate="desactivar" @signal="disminuirSaldo" class="boton" :class="desactivarBoton"  nombre="Disminuir Saldo"></ModificarSaldo>
     </div>
 </template>
 
 <script>
+import ModificarSaldo from './ModificarSaldo'
 export default {
-
+    name:'Cuenta',
+    data() {
+        return {
+            titular:'Pablo',
+            cuenta:'visa',
+            saldo: 1000,
+            estado: true,
+            transacciones:['Deposito','Extraccion', 'Cheques'],
+            desactivar:false
+        }
+    },
+    components: {
+        ModificarSaldo
+    },
+    methods: {
+        agregarSaldo() {
+            this.saldo += 100
+            this.desactivar=false
+        },
+        disminuirSaldo() {
+            if(this.saldo === 0){
+                this.desactivar = true
+                
+            }else{
+                this.saldo -= 100
+            }
+            
+        }
+    },
+    computed: {
+        desactivarBoton() {
+            return this.saldo == 0 ? 'boton-desactivado' :''
+        },
+        info(){
+            return !this.desactivar
+        }
+    },
+   
 }
 </script>
 
 <style scoped>
-    div{
+    .container{
         border: solid 1px #35495E;
         width: 600px;
         min-height: 500px;
         border-radius:12px
     }
+    .boton{
+        width: 200px;
+        height: 50px;
+        background: #41B883;
+        color: white;
+        outline: none;
+        border: none;
+        margin: 10px;
+        border-radius: 12px;
+        cursor: pointer;
+    }
+    .boton-desactivado{
+        background: tomato;
+        text-decoration:line-through;
+        cursor:auto;
+    }
+    p{
+        color: red;
+    }
+    
 </style>
--- src/components/ModificarSaldo.vue
+++ src/components/ModificarSaldo.vue
@@ -1,10 +1,19 @@
 <template>
-  
+    <button @click="accion" :disabled="desactivate">{{nombre}}</button>
 </template>
 
 <script>
 export default {
-
+    name:'ModificarSaldo',
+    props: {
+        nombre: String,
+        desactivate: Boolean      
+    },
+    methods: {
+        accion() {
+            this.$emit('signal')
+        }
+    },
 }
 </script>
 
